1270f8c306c055c9d1e46fc6cb6fae6b62f55602
Bureaupixel <bram.overstijns@gmail.com>
2024-11-16 20:12:31 +0100

    Add files via upload

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,42 +1,36 @@
 <template>
-<div>
-  <div class="pr-5">
-    <Drawer />
+  <div class="mb-20 animate-fade animate-once animate-delay-[500ms]">
+    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+      <component
+        :is="getLayoutComponent(data.layoutblog)"
+        :data="data"
+        :formatDate="formatDate"
+      />
+    </ContentQuery>
   </div>
-  <div class="container mx-auto max-w-4xl space-y-6 h-full mb-20 mt-10">
-    <NuxtLink to="/blog">
-      <span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m6.921 12.5l5.793 5.792L12 19l-7-7l7-7l.714.708L6.92 11.5H19v1z"></path></svg>
-      </span>  
-      <span class="text-xs">
-      back to the blog overview
-      </span>
-    </NuxtLink>
-    <h1 class="text-4xl font-bold">
-      {{ post.title }}
-    </h1>
-    <h4 v-if="post.subtitle" class="text-xl font-bold opacity-80">
-      {{ post.subtitle }}
-    </h4>
-    <ContentDoc />
-    <div class="text-xs leading-3">
-      <hr> 
-      <p v-if="post.date" class="text-xs opacity-50 hover:opacity-100">last updated on: {{ formatDate(post.date) }}
-            <span v-if="post.author" class="text-xs">authored by: {{ post.author }} </span>
-          </p>
-      <p v-if="post.tags"><ArticleTags /></p> 
-    </div>
-  </div>
-</div>  
 </template>
 
 <script setup>
-const route = useRoute()
-const actualPath = route.path.replace(/\/$/, '');
+import { ref } from 'vue';
+import { useRoute } from '#app';
 
-const { data: post } = await useAsyncData(`hello`, () =>
-    queryContent(actualPath).findOne()
-);
+import Layout1 from '~/components/layouts/Layout1.vue';
+import Layout2 from '~/components/layouts/Layout2.vue';
+import Layout3 from '~/components/layouts/Layout3.vue';
+
+// Function to get the corresponding layout component
+function getLayoutComponent(layout) {
+  switch (layout) {
+    case 'layout1':
+      return Layout1;
+    case 'layout2':
+      return Layout2;
+    case 'layout3':
+      return Layout3;
+    default:
+      return null;
+  }
+}
 
 // Function to format the date
 function formatDate(date) {
@@ -44,3 +38,7 @@ function formatDate(date) {
   return new Date(date).toLocaleDateString(undefined, options);
 }
 </script>
+
+<style>
+  .tags li{ display: inline; padding-right:.2em; }
+</style>
