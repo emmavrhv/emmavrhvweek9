2a5431a3ac0ea7ab5b35d866f36a6bb162e6597d
Bureaupixel <bram.overstijns@gmail.com>
2024-11-16 20:08:56 +0100

    components

--- components/Navigations.vue
+++ components/Navigations.vue
@@ -1,29 +1,21 @@
 <template>
-    <div class="opacity-80 animate-fade animate-once animate-delay-[10ms] pt-5">
+  <div class="opacity-80 animate-fade animate-once animate-delay-[10ms] pt-5">
 
-      <div v-if="navigations.mainmenu.mainavigation_on_off">
-        <div class="animate-fade animate-once animate-delay-[150ms]">
-            <MainNavigation />
-        </div>
-      </div>
-
-      <div v-if="navigations.submenu.pagenavigation_on_off">
-        <div class="animate-fade animate-once animate-delay-[150ms]">
-            <PageNavigation />
-        </div>
-      </div>
-      <div v-if="navigations.submenu.blognavigation_on_off">
-        <div class="animate-fade animate-once animate-delay-[150ms]">
-            <BlogNavigation />
-        </div>
+    <div v-if="navigations.mainmenu.mainavigation_on_off">
+      <div class="animate-fade animate-once animate-delay-[150ms]">
+          <MainNavigation />
       </div>
     </div>
+
+
+  </div>
 </template>    
 
 
 
 <script setup>
 import { ref } from 'vue';
+import MainNavigation from './global/navs/MainNavigation.vue';
 
 const navigations = ref({});
 // Use useFetch to load the JSON data from the public folder
@@ -31,9 +23,9 @@ const { data, error } = await useFetch('/_data/menus.json');
 
 // Set the data to the navigations ref
 if (data.value) {
-  navigations.value = data.value;
+navigations.value = data.value;
 } else if (error.value) {
-  console.error('Error loading navigations:', error.value);
+console.error('Error loading navigations:', error.value);
 }
 
 </script>
@@ -41,5 +33,5 @@ if (data.value) {
 
 <style>
 hr{
-  opacity: .1;
+opacity: .1;
 }</style>
