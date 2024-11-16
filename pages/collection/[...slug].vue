3a3d7209ab09b43c42d76cc6d4ba694554b02aa6
Bureaupixel <bram.overstijns@gmail.com>
2024-11-16 21:05:20 +0100

    fourth update

--- pages/collection/[...slug].vue
+++ pages/collection/[...slug].vue
@@ -1,69 +1,40 @@
 <template>
-  <div class="pr-5">
-    <Drawer />
+  <div class="mb-20 animate-fade animate-once animate-delay-[500ms]">
+    <ContentQuery :path="$route.path" find="one" v-slot="{ data }">
+      <component
+        :is="getLayoutComponent(data.layout)"
+        :data="data"
+        :formatDate="formatDate"
+      />
+    </ContentQuery>
   </div>
-  <div class="container mb-20 mt-10 animate-fade animate-once animate-delay-[500ms]"> 
-    <div class="mb-10">
-      <NuxtLink :to="`/${post._dir}`">
-        <span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-            <path fill="currentColor" d="m6.921 12.5l5.793 5.792L12 19l-7-7l7-7l.714.708L6.92 11.5H19v1z"></path>
-          </svg>
-        </span>  
-        <span class="text-xs">Back to pages overview</span>
-      </NuxtLink>
-    </div>
-
-    <ContentSlot >
-      <h1 class="text-4xl font-bold">{{ post.title }}</h1>
-      <p class="text-xl opacity-80" >{{ post.description }}</p>
-    </ContentSlot>  
-
-    <ContentDoc v-slot="{ doc }">
-      <article>
-        <ContentRenderer :value="doc" />
-      </article>
-    </ContentDoc>
-
-    <div v-if="post.imagegallery && post.showgaller !=='false'">
-      <ImageGallery />
-    </div>
-
-
-    <div class="text-xs leading-3">
-      <hr> 
-      <p v-if="post.date" class="text-xs opacity-50 hover:opacity-100">last updated on: {{ formatDate(post.date) }}
-            <span v-if="post.author" class="text-xs">authored by: {{ post.author }} </span>
-          </p>
-      <p v-if="post.tags"><ArticleTags /></p> 
-    </div>
-
-  </div>
-
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useRoute, useAsyncData } from '#app';
+import { ref } from 'vue';
+import { useRoute } from '#app';
 
-// Define reactive properties for managing image loading state
-const imageLoading = ref(true);
+import Layout1 from '~/components/layouts/Layout1.vue';
+import Layout2 from '~/components/layouts/Layout2.vue';
+import Layout3 from '~/components/layouts/Layout3.vue';
 
-// Get the current route
-const route = useRoute();
-const actualPath = route.path.replace(/\/$/, '');
+// Function to get the corresponding layout component
+function getLayoutComponent(layout) {
+  switch (layout) {
+    case 'layout1':
+      return Layout1;
+    case 'layout2':
+      return Layout2;
+    case 'layout3':
+      return Layout3;
+    default:
+      return null;
+  }
+}
 
-// Fetch post data
-const { data: post } = await useAsyncData('page', () =>
-  queryContent(actualPath).findOne()
-);
-
-
-// Format the date
+// Function to format the date
 function formatDate(date) {
   const options = { year: 'numeric', month: 'long' };
   return new Date(date).toLocaleDateString(undefined, options);
 }
 </script>
-
-
