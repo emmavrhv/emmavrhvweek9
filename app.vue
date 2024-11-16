6b975532ee85d4e45acb3115810fd40f36294b14
Bureaupixel <bram.overstijns@gmail.com>
2024-11-16 18:51:59 +0100

    update

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
 <div class="w-full h-full no-print">
-
+    <NuxtLayout>
       <NuxtPage />
-
+    </NuxtLayout>
   </div>
 </template>
-
-<script setup>
-useHead({
-  title: 'Bureaupixel Example Decap & Nuxt',
-  meta: [
-    { name: 'description', content: 'Example for students' }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
-</script>
