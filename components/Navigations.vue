51c96640bd8757661995f84f5415041f99d66f4c
Bureaupixel <bram.overstijns@gmail.com>
2024-11-16 20:42:15 +0100

    first update

--- components/Navigations.vue
+++ components/Navigations.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="opacity-80 animate-fade animate-once animate-delay-[10ms] pt-5">
+    <div class="opacity-80 animate-fade animate-once animate-delay-[10ms] pt-5">
 
-    <div v-if="navigations.mainmenu.mainavigation_on_off">
-      <div class="animate-fade animate-once animate-delay-[150ms]">
-          <MainNavigation />
+      <div v-if="navigations.mainmenu.mainavigation_on_off">
+        <div class="animate-fade animate-once animate-delay-[150ms]">
+            <MainNavigation />
+        </div>
       </div>
+
     </div>
-
-
-  </div>
 </template>    
-
-
 
 <script setup>
 import { ref } from 'vue';
-import MainNavigation from './global/navs/MainNavigation.vue';
 
 const navigations = ref({});
 // Use useFetch to load the JSON data from the public folder
@@ -23,15 +19,15 @@ const { data, error } = await useFetch('/_data/menus.json');
 
 // Set the data to the navigations ref
 if (data.value) {
-navigations.value = data.value;
+  navigations.value = data.value;
 } else if (error.value) {
-console.error('Error loading navigations:', error.value);
+  console.error('Error loading navigations:', error.value);
 }
-
 </script>
 
 
 <style>
 hr{
-opacity: .1;
-}</style>
+  opacity: .1;
+}
+</style>
