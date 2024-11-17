c158426e6848d180a78bad6abfa4bcd0475394db
Bureaupixel <bram.overstijns@gmail.com>
2024-11-17 19:26:08 +0100

    lazy load

--- components/global/ImageGallery.vue
+++ components/global/ImageGallery.vue
@@ -12,26 +12,24 @@
         </p>
         <p v-if="post.imagegallery.gallerytext" class="pb-2">{{ post.imagegallery.gallerytext }}</p>
 
-
         <el-carousel indicator-position="none" motion-blur>
-    <el-carousel-item v-for="(item, index) in post.imagegallery.galleryImages" :key="index">
-      <div class="relative h-full flex items-center justify-center">
-        <!-- Show loader until the image is fully loaded -->
-        <div v-if="imageLoading[index]" class="absolute inset-0 flex items-center justify-center bg-gray-200">
-          <p>Loading image...</p>
-        </div>
-        <NuxtImg
-          :src="item"
-          alt="Gallery Image"
-          @load="imageLoaded(index)"
-          @click="openFullscreen(item)"
-          class="w-full h-auto max-h-full object-contain"
-        />
-      </div>
-    </el-carousel-item>
-  </el-carousel>
-
-
+          <el-carousel-item v-for="(item, index) in post.imagegallery.galleryImages" :key="index">
+            <div class="relative h-full flex items-center justify-center">
+              <!-- Show loader until the image is fully loaded -->
+              <div v-if="imageLoading[index]" class="absolute inset-0 flex items-center justify-center bg-gray-200">
+                <p>Loading image...</p>
+              </div>
+              <NuxtImg
+                :src="item"
+                alt="Gallery Image"
+                loading="lazy"
+                @load="imageLoaded(index)"
+                @click="openFullscreen(item)"
+                class="w-full h-auto max-h-full object-contain"
+              />
+            </div>
+          </el-carousel-item>
+        </el-carousel>
       </div>
 
       <!-- Grid -->
@@ -52,6 +50,7 @@
               <NuxtImg
                 :src="item"
                 alt="Gallery Image"
+                loading="lazy"
                 @click="openFullscreen(item)"
                 @load="imageLoaded(index)"
                 class="cursor-pointer w-full object-cover rounded-md"
@@ -63,14 +62,17 @@
         <!-- Fullscreen Overlay -->
         <div v-if="isOverlayVisible" class="fullscreen-overlay" @click="closeFullscreen">
           <NuxtImg :src="currentImage" alt="Fullscreen Image" class="fullscreen-image" />
-          <button @click="closeFullscreen" class="close-button">            
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"></path></svg>
+          <button @click="closeFullscreen" class="close-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+              <path fill="currentColor" d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"></path>
+            </svg>
           </button>
         </div>
       </div>
     </div>
   </ClientOnly>
 </template>
+
 
 <script setup>
 import { ref, onMounted } from 'vue';
